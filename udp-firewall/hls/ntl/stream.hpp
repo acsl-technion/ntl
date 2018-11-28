bc04ef6a67a3727659871ddb54e1de13af0264ba
Haggai Eran <haggai.eran@gmail.com>
2018-11-28 10:46:33 +0200

    ntl/stream: specialize for different interfaces
    The compiler sometimes cannot infer that only one of write/write_nb is
    going to be called, so help it by specializing the entire class.

--- udp-firewall/hls/ntl/stream.hpp
+++ udp-firewall/hls/ntl/stream.hpp
@@ -3,45 +3,87 @@
 #include <hls_stream.h>
 
 namespace ntl {
-    template <typename T, bool axi_write = false, bool axi_read = false>
-    class stream
+    struct ap_fifo_tag {};
+    struct axi_stream_input_tag {};
+    struct axi_stream_output_tag {};
+
+    namespace detail {
+        template <typename T>
+        class stream_common {
+        public:
+            stream_common() : _stream() {}
+
+            bool empty()
+            {
+    #pragma HLS inline
+                return _stream.empty();
+            }
+
+        protected:
+            hls::stream<T> _stream;
+        };
+    }
+
+    template <typename T, typename Kind = ap_fifo_tag> class stream;
+
+    template <typename T>
+    class stream<T, ap_fifo_tag> : public detail::stream_common<T>
     {
     public:
-        stream() : _stream() {}
+        typedef detail::stream_common<T> base;
 
         template <typename U>
         void write(const U& u)
         {
-            if (axi_write)
-                _stream.write(u);
-            else
-                _stream.write_nb(u);
+#pragma HLS inline
+            base::_stream.write_nb(u);
         }
 
         T read()
         {
+#pragma HLS inline
             T t;
-            if (axi_read) {
-                t = _stream.read();
-            } else {
-                _stream.read_nb(t);
-            }
+            base::_stream.read_nb(t);
             return t;
-        }
-
-        bool empty()
-        {
-            return _stream.empty();
         }
 
         bool full()
         {
-            if (axi_write)
-                return false;
-            else
-                return _stream.full();
+#pragma HLS inline
+            return base::_stream.full();
         }
-    private:
-        hls::stream<T> _stream;
+    };
+
+    template <typename T>
+    class stream<T, axi_stream_input_tag> : public detail::stream_common<T>
+    {
+    public:
+        typedef detail::stream_common<T> base;
+
+        T read()
+        {
+#pragma HLS inline
+            return base::_stream.read();
+        }
+    };
+
+    template <typename T>
+    class stream<T, axi_stream_output_tag> : public detail::stream_common<T>
+    {
+    public:
+        typedef detail::stream_common<T> base;
+
+        template <typename U>
+        void write(const U& u)
+        {
+#pragma HLS inline
+            base::_stream.write(u);
+        }
+
+        bool full()
+        {
+#pragma HLS inline
+            return false;
+        }
     };
 }
