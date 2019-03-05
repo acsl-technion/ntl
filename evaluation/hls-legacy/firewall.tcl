1f68a88269ba7954488d5c0a44d1c832e0321398
Haggai Eran <haggai.eran@gmail.com>
2019-03-05 17:44:22 +0200

    build: generate all HLS projects with a common HLS target
    Refactor scripts to have common code for building HLS targets.

--- evaluation/hls-legacy/firewall.tcl
+++ evaluation/hls-legacy/firewall.tcl
@@ -1,38 +1,6 @@
 package require Tcl 8.5
+set basename [info script]
+set basedir [file join [pwd] {*}[lrange [file split $basename] 0 end-1]]
+source "$basedir/../../examples/common.tcl"
 
-proc create_project {name top dir files tb_files} {
-    open_project -reset "$name"
-
-    global env
-    set_top $top
-    set GTEST_ROOT $::env(GTEST_ROOT)
-    set cflags "-std=gnu++0x -Wno-gnu-designator \
-                -I$GTEST_ROOT/include"
-
-    set ldflags "-lpcap -L$GTEST_ROOT -lgtest"
-
-    foreach f $files {
-        set f [file join $dir $f]
-        add_files $f -cflags $cflags
-    }
-
-    foreach f $tb_files {
-        if {![file exists $f]} {
-            set f [file join $dir $f]
-        }
-        add_files -tb $f -cflags $cflags
-    }
-
-    open_solution "40Gbps"
-    set_part {xcku060-ffva1156-2-i}
-    create_clock -period "216.25MHz"
-    config_rtl -prefix ${name}_
-    config_interface -m_axi_addr64
-    if {[llength $tb_files] > 0} {
-        csim_design -ldflags $ldflags
-    }
-    csynth_design
-    export_design -format ip_catalog
-}
-
-create_project firewall {firewall_top} . {firewall.cpp} {main.cpp}
+create_project firewall {firewall_top} $basedir {firewall.cpp} {main.cpp}
