e5b7017ef511109ab2454b97ff6d5530a1610dde
Haggai Eran <haggai.eran@gmail.com>
2019-03-05 17:36:19 +0200

    scripts: update cloc scripts to new dirs

--- evaluation/scripts/cloc.sh
+++ evaluation/scripts/cloc.sh
@@ -12,17 +12,13 @@ cmd cloc --force-lang=C++,p4 $dir/../p4/*.p4
 echo ----
 
 echo Firewall:
-cmd cloc $(find $dir/../hls/ -maxdepth 1 -name '*.[hc]pp' -not -name main.cpp)
+cmd cloc $(find $dir/../../examples/udp-firewall/ -maxdepth 1 -name '*.[hc]pp' -not -name main.cpp)
 echo ----
 
 echo Library:
-cmd cloc $dir/../hls/ntl/
+cmd cloc $dir/../../ntl/
 echo ----
 
 echo Legacy firewall:
-cmd cloc $(find $dir/../hls-legacy/ -maxdepth 1 -name '*.[hc]pp' -not -name main.cpp)
-echo ----
-
-echo Legacy library:
-cmd cloc $dir/../hls-legacy/ntl/
+cmd cloc $(find $dir/../hls-legacy/ -name '*.[hc]pp' -not -name main.cpp)
 echo ----
